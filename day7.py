23925eaf5131f5c909a928ebf0f606537ad8d628
Shady <shadyelgewily@gmail.com>
2020-12-08 13:05:36 +0100

    Day 7b\

--- day7.py
+++ day7.py
@@ -13,6 +13,36 @@ class BagRules():
     def solve_part_a(self):
         return len(nx.ancestors(self.graph, 'shiny gold'))
 
+    """Consider only the subtree from shiny gold downwards. Each node occurs only once in the network, i.e. the network is not circular
+       So the idea is for each node in the subtree, to:
+    - find all possible paths to shiny gold
+    - traverse each of the paths in turn until you reach shiny gold
+    - multiply the weights along the way
+    - add those numbers to a total sum before going to the next path
+    - When all paths are traversed for a particular node, go to the next node and repeat
+    """
+    def solve_part_b(self):
+        relevant_subtree = nx.dfs_tree(self.graph, 'shiny gold')
+        relevant_nodes = relevant_subtree.nodes - ['shiny gold']
+        total_sum = 0
+        for n in relevant_nodes:
+            total_sum = total_sum + self.calc_contribution_to_sum_of_node(n)
+        return total_sum
+
+    def calc_contribution_to_sum_of_node(self, node):
+        possible_paths = list(nx.all_simple_paths(self.graph, 'shiny gold', node))
+        total_sum = 0
+        for path in possible_paths:
+            total_sum = total_sum + self.calc_contribution_to_sum_of_path(path)
+        return total_sum
+
+    def calc_contribution_to_sum_of_path(self, path):
+        path_steps = [(path[i], path[i + 1]) for i in range(0, len(path) - 1)]
+        contribution_to_sum = 1
+        for source, dest in path_steps:
+            contribution_to_sum = contribution_to_sum * self.graph.get_edge_data(source, dest)['weight']
+        return contribution_to_sum
+
     def process_rules(self, filename):
         self.read_rules(filename)
         self.create_graph_of_bag_rules()
@@ -58,7 +88,13 @@ class BagRules():
 def run_unit_tests():
     bag_rules = BagRules('Data/input_day7a_test.txt')
     assert bag_rules.solve_part_a() == 4
+    assert bag_rules.solve_part_b() == 32
+
+    bag_rules = BagRules('Data/input_day7b_test.txt')
+    assert bag_rules.solve_part_b() == 126
+
 
 run_unit_tests()
 bag_rules = BagRules('Data/input_day7.txt')
 bag_rules.solve_part_a()
+bag_rules.solve_part_b()
