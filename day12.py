9b50828aa2041d3ac1316a0f862a3c2774d82e8e
Shady <shadyelgewily@gmail.com>
2020-12-17 11:28:42 +0100

    Day 12, part b

--- day12.py
+++ day12.py
@@ -1,5 +1,10 @@
 import operator
 import ipdb
+import numpy as np
+
+"""It might be better to use Composition rather than inheritance here, but we know the program isn't going to be more complicated than this, 
+so I am satisfied with this approach. Because we are only moving in 4 directions, the current approach for Left and Right rotation is fine.
+Otherwise, we would need to be more creative mathematically."""
 
 class Ferry():
     def __init__(self, filename):
@@ -44,12 +49,57 @@ class Ferry():
     def get_manhattan_distance(self):
         return abs(self.position_history[-1][0]) + abs(self.position_history[-1][1])
 
+class FerryPartB(Ferry):
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.waypoint = np.array([1,10])
+
+    def action_N(self, distance):
+        self.waypoint = self.waypoint + np.array([distance, 0])
+
+    def action_S(self, distance):
+        self.waypoint = self.waypoint + np.array([-distance, 0])
+
+    def action_E(self, distance):
+        self.waypoint = self.waypoint + np.array([0, distance])
+
+    def action_W(self, distance):
+        self.waypoint = self.waypoint + np.array([0, -distance])
+
+    def action_L(self, degrees):
+        if degrees == 90:
+            self.waypoint = np.array([self.waypoint[1], -self.waypoint[0]] )
+        elif degrees == 180:
+            self.waypoint = np.array([-self.waypoint[0], -self.waypoint[1]])
+        elif degrees == 270:
+            self.waypoint = np.array([-self.waypoint[1], self.waypoint[0]])
+
+    def action_R(self, degrees):
+        if degrees == 90:
+            self.waypoint = np.array([-self.waypoint[1], self.waypoint[0]] )
+        elif degrees == 180:
+            self.waypoint = np.array([-self.waypoint[0], -self.waypoint[1]])
+        elif degrees == 270:
+            self.waypoint = np.array([self.waypoint[1], -self.waypoint[0]])
+
+    def action_F(self, distance):
+        self.position_history.append(tuple(map(operator.add, self.position_history[-1], distance * self.waypoint)))
+
 def unit_tests():
     ferry = Ferry('Data/input_day12a_test.txt')
     ferry.follow_instructions()
     assert ferry.get_manhattan_distance() == 25
 
+    ferryB = FerryPartB('Data/input_day12a_test.txt')
+    ferryB.follow_instructions()
+    assert ferryB.get_manhattan_distance() == 286
+
 unit_tests()
+
 ferry = Ferry('Data/input_day12.txt')
 ferry.follow_instructions()
 ferry.get_manhattan_distance()
+
+ferryB = FerryPartB('Data/input_day12.txt')
+ferryB.follow_instructions()
+ferryB.get_manhattan_distance()
